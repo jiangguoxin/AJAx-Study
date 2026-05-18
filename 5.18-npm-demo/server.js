44d2d2693f06f8f8a7618c7b1b91742526c0f342
jiangguoxin <1778208267@qq.com>
2026-05-18 21:14:03 +0800

    npm的使用，下载了dayjs作为例子

---
+++ 5.18-npm-demo/server.js
@@ -0,0 +1,11 @@
+/**
+ * 目标：使用npm下载dayjs软件包格式化日期时间、
+ * 初始化项目清单文件package.json，npm init -y
+ * 下载软件包到当前项目，npm i dayjs
+ * 使用软件包
+ */
+
+// 使用
+const dayjs = require('dayjs');
+const nowDateStr = dayjs().format('YYYY-MM-DD HH:mm:ss');
+console.log(nowDateStr);
